40d08dcce154bd1a0cc8be6b6eae5ed320b83623
Stephen Maxwell <stephenmaxwell@Stephens-MacBook-Air.local>
2024-05-13 15:39:46 +0100

    fix links

--- js/rotatingquotes.js
+++ js/rotatingquotes.js
@@ -1,6 +1,5 @@
 var QofD = new Array(
   'You can easily judge the character of a man by how he treats those who can do nothing for him.</br>- Johann Wolfgang von Goethe',
-   'Nothing will benefit human health and increase the chances for survival of life on earth as much as the evolution to a vegetarian diet.</br>- Albert Einstein',
    'If a group of beings from another planet were to land on Earth - beings who considered themselves as superior to you as you feel yourself to be to other animals - would you concede them the rights over you that you assume over other animals?</br>- George Bernard Shaw',
     'But for the sake of some little mouthful of flesh we deprive a soul of the sun and light, and of that proportion of life and time it had been born into the world to enjoy.</br>- Plutarch',
     'I am not interested to know whether vivisection produces results that are profitable to the human race or doesn\'t...The pain which it inflicts upon unconsenting animals is the basis of my enmity toward it, and it is to me sufficient justification of the enmity without looking further.</br>- Mark Twain',
